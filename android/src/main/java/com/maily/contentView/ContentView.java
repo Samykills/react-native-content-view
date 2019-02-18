53ba1ca5dfe0de2aa14079d61712591a4d527c52
ugupta <ullas.g@smartenspaces.com>
2019-02-19 00:18:07 +0530

    **Changes** : adding JS caller util **Developer : Ullas Gupta**

--- android/src/main/java/com/maily/contentView/ContentView.java
+++ android/src/main/java/com/maily/contentView/ContentView.java
@@ -1,28 +1,33 @@
 package com.maily.contentView;
 
-import android.content.Context;
 import android.graphics.Color;
 import android.support.annotation.NonNull;
-import android.text.method.ScrollingMovementMethod;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.facebook.react.bridge.ReactContext;
+import com.maily.contentView.utils.Util;
 
 public class ContentView extends LinearLayout {
 
     private View view = null;
     private Button buttonView = null;
     private TextView textView = null;
+    private ReactContext reactContext = null;
 
-    public ContentView(@NonNull Context context) {
+    public ContentView(@NonNull ReactContext context) {
         super(context);
+        reactContext = context;
         view = View.inflate(context, R.layout.content_view, this);
         buttonView = view.findViewById(R.id.contentViewButton);
         buttonView.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                Util.callJSfunction("onPressToggleReadButton", null, reactContext);
+                Toast.makeText(reactContext, "Pressed", Toast.LENGTH_SHORT).show();
             }
         });
         textView = view.findViewById(R.id.contentViewText);
--- android/src/main/java/com/maily/contentView/utils/Util.java
+++ android/src/main/java/com/maily/contentView/utils/Util.java
@@ -1,5 +1,29 @@
 package com.maily.contentView.utils;
 
+import com.facebook.react.bridge.CatalystInstance;
+import com.facebook.react.bridge.ReactContext;
+import com.facebook.react.bridge.WritableNativeArray;
+import com.google.gson.Gson;
+
 public class Util {
+    private static final String LinkerUtil = "LinkerUtil";
+    private static Gson gson = new Gson();
+
+    /**
+     * This is used to invoke a JS function from native code. more suited in case of reactive programming.
+     *
+     * @param functionName
+     * @param param
+     * @param reactContext
+     */
+    public static void callJSfunction(String functionName, Object param, ReactContext reactContext) {
+        CatalystInstance catalystInstance = reactContext.getCatalystInstance();
+        WritableNativeArray params = new WritableNativeArray();
+        if (param != null) {
+            String jsonObject = gson.toJson(param);
+            params.pushString(jsonObject);
+        }
+        catalystInstance.callFunction(LinkerUtil, functionName, params);
+    }
 
 }
